3c9cd03a79ca7cd39a24184b9d55c62c9f500e9d
Timothée Picard <timothee@atnos.com>
2022-05-31 13:01:35 +0200

    create experience and education components

---
+++ src/components/sections/SectionExperience.vue
@@ -0,0 +1,75 @@
+<template>
+  <section>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tincidunt velit a enim dictum, nec euismod
+      erat feugiat. Duis ut enim ac augue pretium vehicula vel vel lectus. Sed placerat mauris felis, non bibendum
+      nunc cursus eu. Nulla pharetra suscipit elementum. Vestibulum in nisl eu sapien feugiat fringilla. Curabitur
+      suscipit vestibulum felis, in ullamcorper magna vulputate at. Etiam sit amet sem id nunc eleifend consequat.
+      Curabitur eget nibh fermentum, eleifend massa ut, vehicula orci. Nunc id dapibus nisi. Donec posuere odio sit
+      amet eros eleifend, sed dictum tortor ullamcorper. Maecenas at velit mollis, efficitur leo sed, egestas nisi.
+      Sed aliquam eleifend est, eget posuere eros scelerisque in.
+
+      Morbi pulvinar malesuada libero non congue. Cras sed lectus eget neque mollis consectetur non et justo. Curabitur
+      sit amet turpis rutrum, blandit massa ac, hendrerit odio. In lacinia enim orci, sit amet hendrerit sapien maximus
+      quis. Duis posuere dui sed orci bibendum, at condimentum sem porttitor. Proin porta iaculis blandit. Sed non orci
+      sollicitudin, sagittis elit sed, fringilla erat. Orci varius natoque penatibus et magnis dis parturient montes,
+      nascetur ridiculus mus.
+
+      Fusce quis magna tortor. Sed vitae velit vitae lorem elementum pellentesque. Sed sagittis dignissim pellentesque.
+      Donec pharetra justo vel sollicitudin fringilla. Praesent porttitor ac urna ut congue. Pellentesque aliquam augue
+      at faucibus finibus. Vestibulum pharetra hendrerit rhoncus. Quisque semper felis est, id luctus risus placerat id.
+      Curabitur lacinia sodales neque, rutrum pulvinar sem hendrerit nec. In pretium dolor tincidunt dui mollis aliquam.
+      Donec eget iaculis lacus. Praesent egestas aliquet velit, rutrum feugiat tortor lacinia eget. Nulla non
+      pellentesque leo. Proin ac tortor dignissim, vehicula lorem non, elementum mi. Sed orci eros, porttitor efficitur
+      velit sed, euismod dignissim ligula.
+    </p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "SectionExperience"
+}
+</script>
+
+<style scoped lang="scss">
+@import "@/assets/var";
+section {
+  position: relative;
+  background: $color-dark-green;
+  margin-bottom: 50px;
+  padding: 7em 0 5em;
+  &::before {
+    content: '';
+    position: absolute;
+    top: -3px;
+    width: 0;
+    height: 0;
+    border-style: solid;
+    border-width: 50px 70px 0 70px;
+    border-color: $color-white transparent transparent transparent;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+  &::after {
+    content: '';
+    position: absolute;
+    bottom: 3px;
+    width: 0;
+    height: 0;
+    border-style: solid;
+    border-width: 50px 70px 0 70px;
+    border-color: $color-dark-green transparent transparent transparent;
+    left: 50%;
+    transform: translateX(-50%) translateY(100%);
+  }
+}
+
+section {
+  padding: 5em 0;
+  p {
+    max-width: 900px;
+    margin: auto;
+  }
+}
+</style>
