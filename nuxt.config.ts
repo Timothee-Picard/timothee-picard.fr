139cdad23216d448eabb908b3b619fbdea6260a0
Timothee-Picard <timothee@atnos.com>
2022-11-17 17:16:33 +0100

    add cv page and update title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    modules: ['nuxt-icon']
+    modules: ['nuxt-icon'],
+    app: {
+        head: {
+            charset: 'utf-16',
+            viewport: 'width=500, initial-scale=1',
+            title: 'timothee-picard.fr',
+            meta: [
+                { name: 'description', content: 'Portfolio de timothée picard développeur web frontend' }
+            ],
+        }
+    }
 });
---
+++ pages/cv.vue
@@ -0,0 +1,3 @@
+<template>
+  <iframe src="/CV_Timothée%20Picard.pdf" style="width:100%; min-height: 101vh;" frameborder="0" allowfullscreen></iframe>
+</template>
